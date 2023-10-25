61bd325921ee2cafc8f35abb592870269b8439fd
cristian-dinu-69 <cristian69dinu@gmail.com>
2023-10-26 00:48:24 +0300

    modified function index to january and added february url + created function

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -24,7 +24,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index)
+    path("january", views.january),
+    path("february", views.february)
 
 ]
 
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return  HttpResponse("This Works") # (something : response to send back to the client) - param request - HttpResponse - 
+def january(request):
+    return  HttpResponse("This Works - Eat no meat for the entire month") # (something : response to send back to the client) - param request - HttpResponse - 
                                        # need to import:  from django.http import HttpResponse
                                        # as an argument a respond data that can be a html page or a string
+
+def february(requst):
+    return HttpResponse("Feb Response - Walk for at least 20 minuts every day")
